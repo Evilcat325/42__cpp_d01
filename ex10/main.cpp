38c9e98845eef738dc8c20b43ef66815ec77aa1a
Sen Li <seli@e1z3r4p24.42.us.org>
2019-05-01 23:40:42 -0700

    add permission check

--- ex10/main.cpp
+++ ex10/main.cpp
@@ -1,6 +1,9 @@
 #include <fstream>
 #include <iostream>
 
+// check if dir is very system dependent
+// c++ file_system was introduced in C++17 so we can't use it here
+// would need to fail back on c if want to check dir or stuff
 int main(int argc, char *argv[])
 {
 	std::string line;
@@ -15,7 +18,7 @@ int main(int argc, char *argv[])
 		std::ifstream fin;
 		fin.open(argv[i]);
 		if (!fin.is_open())
-			std::cerr << "cat9tails: " << argv[i] << ": No such file or directory" << std::endl;
+			std::cerr << "cat9tails: " << argv[i] << ": No such file or directory or permission failed" << std::endl;
 		else
 		{
 			while (std::getline(fin, line))
